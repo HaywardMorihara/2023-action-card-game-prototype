d3f128072468176e199144d72ed4a7715aed01fb
HaywardMorihara <nathanielmorihara@gmail.com>
2023-04-09 14:25:20 -0400

    Introduce Player into the World

---
+++ player/Player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+@export var speed = 100
+
+@onready var animation = get_node("AnimatedSprite2D")
+
+func _physics_process(delta):
+	velocity = _get_input_direction() * speed
+	move_and_slide()
+	_animate()
+	
+
+func _get_input_direction() -> Vector2:
+	# Auto-normalized
+	return Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+
+func _animate():
+	if velocity.is_zero_approx():
+		animation.play("idle")
+	else:
+		if abs(velocity.x) > abs(velocity.y):
+			if velocity.x > 0:
+				animation.play("walk_right")
+			else:
+				animation.play("walk_left")
+		else:
+			if velocity.y > 0:
+				animation.play("walk_down")
+			else:
+				animation.play("walk_up")
+				

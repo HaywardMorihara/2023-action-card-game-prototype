2f31cba4a1257427a8e683476924b5d107fb8dc1
HaywardMorihara <nathanielmorihara@gmail.com>
2023-04-18 08:53:09 -0400

    Playtesting changes - tilemap, blob tweaks

--- world/npcs/enemies/Blob.gd
+++ world/npcs/enemies/Blob.gd
@@ -3,10 +3,10 @@ extends CharacterBody2D
 signal blob_drop(pickup)
 
 @export var speed = 50
-@export var detection_radius = 100
+@export var detection_radius = 200
 @export var damage_to_player = 1
 @export var draw_drop_probability : float = 0.25
-@export var new_card_drop_probability : float = 0.1
+@export var new_card_drop_probability : float = 0.25
 
 @onready var animation : AnimatedSprite2D = get_node("AnimatedSprite2D")
 

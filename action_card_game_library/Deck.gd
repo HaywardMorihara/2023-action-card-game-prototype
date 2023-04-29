c42c0bc7907338a8ebb9fc16c8882023da91e720
HaywardMorihara <nathanielmorihara@gmail.com>
2023-04-28 21:08:22 -0400

    Deck should look like a stack

--- action_card_game_library/Deck.gd
+++ action_card_game_library/Deck.gd
@@ -3,10 +3,14 @@ extends Area2D
 signal card_drawn(card_id : ActionCardGameGlobal.CardId)
 signal deck_discard(card_id : ActionCardGameGlobal.CardId)
 
+@export var is_click_to_draw : bool = false
+
+@onready var bottom_card : Sprite2D = get_node("Card1")
+@onready var middle_card : Sprite2D = get_node("Card2")
+@onready var top_card : Sprite2D = get_node("Card3")
+
 var current_contents : Array[ActionCardGameGlobal.CardId]
 var total_cards : int
-
-@export var is_click_to_draw : bool = false
 
 func _ready():
 	if ActionCardGameGlobal.starting_deck:
@@ -16,31 +20,30 @@ func _ready():
 	if ActionCardGameGlobal.starting_hand_count:
 		for i in ActionCardGameGlobal.starting_hand_count:
 			draw_next_card()
-
+	update_visibility()
 
 func shuffle():
 	randomize()
 	current_contents.shuffle()
-	
 
 func add_card_to_bottom_of_deck(card_id : ActionCardGameGlobal.CardId):
 	if current_contents.size() == 0:
 		visible = true
 	current_contents.push_back(card_id)
-
+	update_visibility()
 
 func add_card_to_front_of_deck(card_id : ActionCardGameGlobal.CardId):
 	if current_contents.size() == 0:
 		visible = true
 	current_contents.push_front(card_id)
-
+	update_visibility()
 
 func draw_next_card():
 	var next_card_id = current_contents.pop_front()
 	card_drawn.emit(next_card_id)
 	if current_contents.size() == 0:
 		visible = false
-	
+	update_visibility()
 	
 func discard_from_top(num_of_cards : int):
 	if current_contents.size() == 0:
@@ -49,7 +52,27 @@ func discard_from_top(num_of_cards : int):
 	deck_discard.emit(next_card_id)
 	if current_contents.size() == 0:
 		visible = false
+	update_visibility()
 
+func update_visibility():
+	match current_contents.size():
+		2:
+			bottom_card.visible = true	
+			middle_card.visible = true	
+			top_card.visible = false
+		1:
+			bottom_card.visible = true	
+			middle_card.visible = false	
+			top_card.visible = false
+		0:
+			bottom_card.visible = false	
+			middle_card.visible = false	
+			top_card.visible = false
+		_:
+			bottom_card.visible = true	
+			middle_card.visible = true	
+			top_card.visible = true	
+	
 
 func _on_input_event(viewport, event, shape_idx):
 	if is_click_to_draw:

941a86c4410f4e19d49681c42dcb5e8327991547
HaywardMorihara <nathanielmorihara@gmail.com>
2023-05-05 19:00:02 -0400

    Fireball Juice

--- world/effects/Fireball.gd
+++ world/effects/Fireball.gd
@@ -5,6 +5,8 @@ extends CharacterBody2D
 # https://docs.godotengine.org/en/4.0/tutorials/physics/using_character_body_2d.html
 # NOT https://kidscancode.org/godot_recipes/4.x/2d/2d_shooting/ (because not official)
 
+var is_queued_destroy := false
+
 func start(_position : Vector2, _direction : float):
 	rotation = _direction
 	position = _position
@@ -13,13 +15,23 @@ func start(_position : Vector2, _direction : float):
 
 # https://docs.godotengine.org/en/3.2/tutorials/physics/physics_introduction.html#collision-layers-and-masks
 func _physics_process(delta):
+	if is_queued_destroy:
+		return
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		var body = collision.get_collider()
 		if body.is_in_group("Enemies"):
 			body.queue_destroy()
-		queue_free()
+		$ExplosionSFX.play()
+		is_queued_destroy = true
+		$TailCPUParticles2D.queue_free()
+		$BodyCPUParticles2D.queue_free()
+		$ExplosionCPUParticles2D.emitting = true
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+
+
+func _on_explosion_sfx_finished():
 	queue_free()

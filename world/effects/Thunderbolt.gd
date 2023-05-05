9dbe53345cae149c10b8a2712f9a7a612b2370dc
HaywardMorihara <nathanielmorihara@gmail.com>
2023-05-04 22:38:00 -0400

    Thunderbolt Juice

--- world/effects/Thunderbolt.gd
+++ world/effects/Thunderbolt.gd
@@ -7,4 +7,23 @@ func _on_animated_sprite_2d_animation_finished():
 		if body_in_blast_radius.is_in_group("Enemies"):
 			body_in_blast_radius.queue_destroy()
 	thunderbolt_strike_finished.emit()
+	$DelayQueueFreeTimer.start()
+	visible = false
+
+
+func _on_thunder_sfx_delay_timer_timeout():
+	$ThunderSFX.play()
+	# TODO Tweak, didn't look right, maybe blobs will take care of this
+#	get_tree().paused = true
+#	$HitFrameTimer.start()
+
+func _on_delay_queue_free_timer_timeout():
 	queue_free()
+
+
+func _on_first_bolt_interruption_timer_timeout():
+	pass
+
+
+func _on_first_bolt_delay_timer_timeout():
+	$FastThunderSFX.play()
